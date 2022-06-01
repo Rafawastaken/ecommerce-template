2a594f96f2aeb3d93bb0d0bb58e3ea404c456423
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-01 22:19:16 +0100

    Register page

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,12 +1,20 @@
-from flask import render_template, session, url_for, redirect
+from flask import render_template, session, url_for, redirect, request, flash
 
 # Local imports
+from .forms import RegistrationForm
 from shop import app, db
 
 @app.route("/")
 def home():
     return "home page of website"
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title = "Register Page")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # user = User(form.username.data, form.email.data,
+        #             form.password.data)
+        # db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title = "Register Page")
