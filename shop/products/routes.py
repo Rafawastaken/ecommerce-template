85a190b3c72bd5abba6e05080b2c4c6260c7d220
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-03 04:07:02 +0100

    add product to database

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,17 +1,23 @@
 from flask import redirect, render_template, url_for, flash, request
 
+import secrets
+
 from .forms import Addproducts
-from .models import Brand, Category
+from .models import Brand, Category, Addproduct
 from shop import db, app, photos
 
 # Add Brands
 @app.route('/addbrand', methods = ['GET', 'POST'])
 def addbrand():
     if request.method == 'POST':
+        # Query brand
         getbrand = request.form.get('brand').title()
+        
+        # Add brand to database
         brand = Brand(name = getbrand)
         db.session.add(brand)
         db.session.commit()
+
         flash(f"Brand: '{getbrand}' added to database!", "success")
         return redirect(url_for('addbrand'))
     return render_template('products/addbrand.html', brands = True, title = 'Add Brand')
@@ -20,10 +26,14 @@ def addbrand():
 @app.route('/addcat', methods = ['GET', 'POST'])
 def addcat():
     if request.method == 'POST':
+        # Query category
         getcategory = request.form.get('category').title()
+
+        # Add Category to database
         cat = Category(name = getcategory)
         db.session.add(cat)
         db.session.commit()
+
         flash(f"Category:'{getcategory}' added to database!", "success")
         return redirect(url_for('addbrand'))
     return render_template('products/addbrand.html', title = 'Add Category')
@@ -36,9 +46,43 @@ def addproduct():
     form = Addproducts(request.form)
 
     if request.method == 'POST':
-        photos.save(request.files.get('image_1'))
-        photos.save(request.files.get('image_2'))
-        photos.save(request.files.get('image_3'))
+        # Get form data
+        name = form.name.data
+        price = form.price.data
+        discount = form.discount.data
+        stock = form.stock.data
+        colors = form.colors.data
+        desc = form.description.data
+
+        # Request form -> Not in forms
+        brand = request.form.get('brand')
+        category = request.form.get('category')
+
+        # Request form media and saves it with hash name
+        image_1 = photos.save(request.files.get('image_1'), name = secrets.token_hex(10) + ".")
+        image_2 = photos.save(request.files.get('image_2'), name = secrets.token_hex(10) + ".")
+        image_3 = photos.save(request.files.get('image_3'), name = secrets.token_hex(10) + ".")
+
+        # Add product to database
+        product = Addproduct(
+            name = name,
+            price = price,
+            discount = discount,
+            stock = stock,
+            colors = colors,
+            desc = desc,
+            brand_id = brand,
+            category_id = category,
+            image_1 = image_1,
+            image_2 = image_2,
+            image_3 = image_3
+        )
+
+        db.session.add(product)
+        db.session.commit()
+
+        flash(f'Product {name} added to store!', 'success')
+        return redirect(url_for('admin'))
 
     return render_template('products/addproduct.html', 
         title = "Add Product Page", form = form, brands = brands, 
