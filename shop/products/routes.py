957a03f4e5f29548ba4abfbbd92ffd3f6b164f26
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-05 00:32:15 +0100

    update product, missing replacing images

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from math import prod
 from flask import redirect, render_template, url_for, flash, request, session
 
 import secrets
@@ -134,3 +135,48 @@ def addproduct():
     return render_template('products/addproduct.html', 
         title = "Add Product Page", form = form, brands = brands, 
         categories = categories)
+
+# Update Product
+@app.route('/updateproduct/<int:id>', methods = ['GET', 'POST'])
+def updateproduct(id):
+    form = Addproducts(request.form)
+    
+    # Query data
+    categories = Category.query.all()
+    brands = Brand.query.all()
+    product = Addproduct.query.get_or_404(id)
+
+    # Aux variables
+    brand_id = request.form.get('brand')
+    category_id = request.form.get('category')
+
+
+    # if form is submitted
+    if request.method == 'POST':
+        # update product in database
+        product.name = form.name.data
+        product.price = form.price.data
+        product.discount = form.discount.data
+        product.stock = form.stock.data
+        product.brand_id = brand_id
+        product.category_id = category_id
+        product.colors = form.colors.data
+        product.desc = form.description.data
+
+        # save on database
+        db.session.commit()
+
+        flash(f"{product.name} updated with success!", 'success')
+        return redirect(url_for('admin'))
+
+        # Match values with form
+    form.name.data = product.name
+    form.price.data = product.price
+    form.discount.data = product.discount
+    form.stock.data = product.stock
+    form.colors.data = product.colors
+    form.description.data = product.desc
+
+    return render_template('products/updateproduct.html', 
+        title = "Update Product", form = form, product = product, 
+        brands = brands, categories = categories)
