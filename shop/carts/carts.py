c46a2a34ce3e8a7c77913827f045f716af3f20d2
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-07 02:09:31 +0100

    update cart, close btn in flash messages

--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -2,6 +2,7 @@ from math import prod
 from operator import sub
 from statistics import quantiles
 from flask import redirect, render_template, request, url_for, flash, session, current_app
+from importlib_metadata import method_cache
 
 from shop import db, app
 from shop.products.models import Addproduct
@@ -31,42 +32,66 @@ def AddCart():
                     'discount':product.discount,
                     'color':colors,
                     'quantity':quantity,
-                    'image':product.image_1
+                    'stock':product.stock,
+                    'image':product.image_1,
+                    'colors':product.colors
                     }
                 }
-
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
                 if product_id in session['Shoppingcart']:
                     print("Product already in cart") # Change to increment.
                 else:
-                    session['Shopppingcart'] = MergeDicts(session['Shopppingcart'], DictItems)
+                    session['Shoppingcart'] = MergeDicts(session['Shoppingcart'], DictItems)
 
                     return redirect(request.referrer)
             else:
-                session['Shopppingcart'] = DictItems
+                session['Shoppingcart'] = DictItems
                 return redirect(request.referrer)
-
     except Exception as e:
         print(e)
     finally:
         return redirect(request.referrer)
 
-
 # Display cart
 @app.route('/carts')
 def getCart():
-    if 'Shopppingcart' not in session:
+    if 'Shoppingcart' not in session:
         return redirect(request.referrer)
-
     subtotal = 0
     grandtotal = 0
-    for key, product in session['Shopppingcart'].items():
+    for key, product in session['Shoppingcart'].items():
         discount = (product['discount']/100) * float(product['price'])
         subtotal = subtotal + float(product['price']) * int(product['quantity'])
         subtotal = subtotal - discount 
         tax = ("%.2f" % (.06 * float(subtotal)))
-
         grandtotal = float("%.2f" % (1.06 * subtotal))
+    return render_template('products/carts.html', title = "Cart", tax = tax, grandtotal = grandtotal)
 
-    return render_template('products/carts.html', tax = tax, grandtotal = grandtotal)
+# Update Cart
+@app.route('/updatecart(<int:code>', methods=['POST'])
+def updatecart(code):
+    # If shopping cart doesnt exist or is empty
+    if 'Shoppingcart' not in session and len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash("Product updated with success!", 'success')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            flash('Something went wrong', 'danger')
+            return redirect(url_for('getCart'))
+
+# Reset card
+@app.route('/reset-cart')
+def delete_cart():
+    session.clear()
+    return redirect(url_for('home'))
