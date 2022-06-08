5fdb07dd41e301b4574d20f27014a49c1dd50112
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-08 15:59:36 +0100

    register model + save database

--- shop/customer/routes.py
+++ shop/customer/routes.py
@@ -7,12 +7,32 @@ import secrets
 import os
 
 # Locals imports
-from shop import db, app, photos, products, search
+from shop import db, app, photos, products, search, bcrypt
 from .forms import CustomerRegisterForm
+from .models import Register
 
 
 # Register Customer Page
 @app.route('/customer/register', methods = ['GET', 'POST'])
 def custom_register():
     form = CustomerRegisterForm(request.form)
+    if request.method == 'POST':
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        register = Register(
+            name = form.name.data,
+            username = form.username.data,
+            email = form.email.data,
+            password = hash_password,
+            country = form.country.data,
+            state = form.state.data,
+            city = form.city.data,
+            address = form.address.data,
+            zipcode = form.zipcode.data,
+        )
+
+        db.session.add(register)
+        db.session.commit()
+
+        flash(f"Welcome {form.name.data}", 'success')
+        return redirect(url_for('home'))
     return render_template('customers/register.html', form = form)
