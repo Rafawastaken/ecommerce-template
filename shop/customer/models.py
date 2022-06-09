b3dfbdc696132394a40e710e374805268bf60dd3
Rafael Pimenta <jrafapimenta@gmail.com>
2022-06-09 01:03:33 +0100

    order model database

--- shop/customer/models.py
+++ shop/customer/models.py
@@ -1,5 +1,6 @@
 from shop import db
 from datetime import datetime
+import json
 
 from shop import login_manager
 from flask_login import UserMixin
@@ -8,7 +9,7 @@ from flask_login import UserMixin
 def user_loader(user_id):
     return Register.query.get(user_id)
 
-
+# Register customer
 class Register(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), unique = False)
@@ -26,5 +27,34 @@ class Register(db.Model, UserMixin):
 
     def __repr__(self):
         return '<Register %r>' % self.name
+
+# Json encoded order
+class JsonEncodeDict(db.TypeDecorator):
+    impl = db.Text
+
+    def set_val(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+
+    def get_value(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.loads(value)
+
+# Customer order
+class CustomerOrder(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    invoice = db.Column(db.String(20), unique = True, nullable = False)
+    status = db.Column(db.String(20), nullable = False, default = 'Pending')
+    customer_id = db.Column(db.Integer, unique = False, nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    orders = db.Column(JsonEncodeDict)
+
+    def __repr__(self):
+        return '<CustomerOrder %r>' % self.invoice
+
 
 db.create_all()
